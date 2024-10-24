7337cdad7ba16cba1bf57fa280d01f18cf668c4f
T-LXY <tonyrms2018@gmail.com>
2024-10-23 21:36:07 -0700

    Scene Transition Finished
    Finished a simple fade scene transition.

---
+++ Assets/Prefabs/SceneLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            LoadNextScene("SceneTransitionTest1");
+        }
+
+    }
+
+    public void LoadNextScene(string sceneName){
+        StartCoroutine(LoadAnimation(sceneName));
+    }
+
+    IEnumerator LoadAnimation(string sceneName){
+        // Play animation
+        transition.SetTrigger("SceneStart");
+
+        // Wait for animation to finish
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load scene
+        SceneManager.LoadScene(sceneName);
+    }
+}
